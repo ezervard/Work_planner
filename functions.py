b7cc130b707d7b751a790529720c653f7bb59b16
ezervard <ezervard@gmail.com>
2023-06-06 15:25:01 +0200

    Initial commit

---
+++ functions.py
@@ -0,0 +1,5 @@
+import calendar
+
+def calenda(year, month):
+    cal = calendar.monthcalendar(year, month)
+    return cal
---
+++ main.py
@@ -0,0 +1,17 @@
+import calendar
+import datetime
+from calendar import  monthcalendar
+from functions import *
+from settings import *
+class work_planner:
+    def __init__(self):
+        pass
+    def init(self):
+        self.functions()
+    def run(self):
+        pass
+
+    print(calenda(2023, 6),)
+    hello()
+if __name__ == "__main__":
+    work_planner.run
